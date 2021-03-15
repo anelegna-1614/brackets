aed911b54f9f6b5ee6ea3d3ce8e0553a01b264af
anelegna-1614 <anelegna-1614@tut.by>
2021-03-15 23:23:58 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str.split('');
+    let i = 0;
+    let x = new Boolean (false);
+    while (i < bracketsConfig.length -1) {
+        let ind1 = arr.indexOf(bracketsConfig[i][0]);
+        let ind2 = arr.indexOf(bracketsConfig[i][1]);
+        if ((arr.length % 2 === 1) || (ind2 - ind1 !== 1) || ((ind2-ind1)%2 === 0)) {
+          x = false;
+          break;
+        }  else {
+          x= true;
+          i = i + 1;
+        } 
+      }return x;
+        
+        }
+        
+      
+  
+
