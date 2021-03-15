12e11654d8407e2c9c9b77316bb353146e61815d
anelegna-1614 <anelegna-1614@tut.by>
2021-03-16 00:34:26 +0300

    fix: change args to fix error

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ module.exports = function check(str, bracketsConfig) {
   let arr = str.split('');
     let i = 0;
     let x = new Boolean (false);
-    while (i < bracketsConfig.length -1) {
+    while (i < bracketsConfig.length) {
         let ind1 = arr.indexOf(bracketsConfig[i][0]);
         let ind2 = arr.indexOf(bracketsConfig[i][1]);
-        if ((arr.length % 2 === 1) || (ind2 - ind1 !== 1) || ((ind2-ind1)%2 === 0)) {
+        if ((arr.length % 2 === 1) || ((ind2-ind1)%2 === 0)) {
           x = false;
           break;
         }  else {
